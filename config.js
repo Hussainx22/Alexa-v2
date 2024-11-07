b2702357b0a8c379488e87e893eff8f031cb2544
Hussainx22 <botx2025@gmail.com>
2024-11-07 18:15:11 +0600

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "YASIYA-MD~xC1iRR4J#w2LnqF87ljJ4LohXsUSvBrGko3WLrm5ueFMucygi65w",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
